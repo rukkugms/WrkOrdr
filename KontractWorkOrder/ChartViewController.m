a9e084a261f4eb7082067a80e6ba842dd7dabc6a
Riya <macindia@Riya.local>
2015-01-28 15:06:15 +0530

    cmmtd

--- KontractWorkOrder/ChartViewController.m
+++ KontractWorkOrder/ChartViewController.m
@@ -33,6 +33,54 @@
     // Pass the selected object to the new view controller.
 }
 */
+#pragma mark-tableview datasource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 6;
+    
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"mycell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        [[NSBundle mainBundle]loadNibNamed:@"ChartcellView" owner:self options:nil];
+        cell=_chartcell;
+    }
+    
+    cell.textLabel.font=[UIFont fontWithName:@"Helvetica Neue" size:12];
+    // cell.textLabel.text=@"WO0001";
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+    //alternating cell back ground color
+    if(tableView==_charttable)
+    {
+        if (indexPath.row%2 == 0) {
+            [cell setBackgroundColor:[UIColor whiteColor]];
+            
+        }else
+        {
+            
+            //[cell setBackgroundColor:[UIColor colorWithRed:247.0/255.0f green:247.0/255.0f blue:247.0/255.0f alpha:1.0f]];
+            [cell setBackgroundColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f]];
+            
+            
+        }
+    }
+}
 
 - (IBAction)addplanbtn:(id)sender {
     self.addplanVCtrl=[[AddplanViewController alloc]initWithNibName:@"AddplanViewController" bundle:nil];
@@ -42,5 +90,8 @@
     [self presentViewController:_addplanVCtrl
                        animated:YES completion:NULL];
     
+}
+- (IBAction)clsebtn:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 @end
